fd236bd4249a5ccfc8a635b6522e489c319d7e88
turbo <turbo>
2003-03-28 11:18:20 +0000

    Configurable key len/alg and ttl in variables - use with dnssec-* commands.

--- dnssec-create_keys.sh
+++ dnssec-create_keys.sh
@@ -1,12 +1,21 @@
 #!/bin/sh
 
-# $Id: dnssec-create_keys.sh,v 1.1 2003-03-28 11:14:32 turbo Exp $
+# $Id: dnssec-create_keys.sh,v 1.2 2003-03-28 11:18:20 turbo Exp $
 
-# Where is the zones files (db.*)?
-DIR_BIND=/etc/bind
+# --------------
+# Set some default variables
+DIR_BIND=/etc/bind			# Where is the zones files (db.*) that Bind9 loads (ie, the signed zone files)?
+					#  - Original zone files are in $DIR_BIND/.original
+DIR_KEYS=/var/cache/bind		# Keyfiles must be in 'directory' clause of named.conf!
 
-# Keyfiles must be in 'directory' clause of named.conf!
-DIR_KEYS=/var/cache/bind
+CURRENTDATE=`date +"%Y%m%d%H%M%S"`	# Current day and time (YYYYMMDDHHMMSS) -> 20030218160510
+
+KEY_TTL=86400				# Key TTL in seconds (86400 -> 24 hours)
+KEY_ALG=RSA				# Key algorithm: RSA | RSAMD5 | DH | DSA | HMAC-MD5
+KEY_LEN=2048				# Key size, in bits:	RSA:		[512..4096]
+					#			DH:		[128..4096]
+					#			DSA:		[512..1024] and divisible by 64
+					#			HMAC-MD5:	[1..512]
 
 # What zones whould we create keys for?
 if [ -z "$1" ]; then
@@ -33,8 +42,8 @@ for zone in $ZONES; do
 
     # Create the keys, keysets and sign the key
     echo -n "$zone"
-    KEY_ZONE=`dnssec-keygen -a DSA -b 768 -n ZONE $zone.` && echo -n "."
-    KEY_SET=`dnssec-makekeyset -t 86400 -s +1 -e +2592000 $KEY_ZONE.key` && echo -n "."
+    KEY_ZONE=`dnssec-keygen -a $KEY_ALG -b $KEY_LEN -n ZONE $zone.` && echo -n "."
+    KEY_SET=`dnssec-makekeyset -t $KEY_TTL -s +1 -e +2592000 $KEY_ZONE.key` && echo -n "."
     KEY_SIGNED=`dnssec-signkey $KEY_SET $KEY_ZONE.private` && echo -n "."
 
     # Sign the zone (replacing the entry '$INCLUDE ...' after the SOA)
