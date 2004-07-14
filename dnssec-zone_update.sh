14b0dcf8bc76aad803849f7e1158e2c0a5100a16
turbo <turbo>
2004-07-14 11:17:46 +0000

    Rewrite - Less hardcoded values (especially the IP etc).

--- dnssec-zone_update.sh
+++ dnssec-zone_update.sh
@@ -1,20 +1,35 @@
 #!/bin/sh
 
+# $Id: dnssec-zone_update.sh,v 1.1 2004-07-14 11:17:46 turbo Exp $
+
 TTL=3600
-SERVER=195.163.1.188
 ZONE=bayour.com
-HOSTNAME=meduza.bayour.com
-KEYFILE=/var/cache/bind/Kmeduza.bayour.com.+157+51061.private
+HOSTNAME=meduza
+NS=212.214.70.50
+
+# Don't change anything below...
+# ------------------------------
+
+KEYFILE=/var/cache/bind/HostKeys/K`echo $HOSTNAME`.`echo $ZONE`*.private
+
+# Get IP on 'external' interface
+TMPFILE=`tempfile --prefix=dns.`
+wget --quiet --output-document=$TMPFILE http://checkip.dyndns.org
+IP=`cat $TMPFILE | sed -e 's@.*: @@' -e 's@<.*@@'`
+rm -f $TMPFILE
+
+exit 1
+
 if [ -z "$1" ]; then
-    new_ip_address=213.67.237.4
+    new_ip_address=$IP
 else
     new_ip_address=$1
 fi
 
 nsupdate -v -k $KEYFILE > /dev/null <<EOF
-server $SERVER
+server $NS
 zone $ZONE.
-update delete $HOSTNAME A
-update add $HOSTNAME $TTL A $new_ip_address
+update delete $HOSTNAME.$ZONE A
+update add $HOSTNAME.$ZONE $TTL A $new_ip_address
 send
 EOF
